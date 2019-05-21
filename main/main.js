7263e7e30e535f84d225aaebf550bb620843222c
rivalove <542763565@qq.com>
2019-05-21 17:22:24 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,24 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+  const mapTop = [`._.`, `...`, `._.`, `._.`, `...`, `._.`, `._.`, `._.`, `._.`, `._.`]
+  const mapCenter = [`|.|`, `..|`, `._|`, `._|`, `|_|`, `|_.`, `|_.`, `..|`, `|_|`, `|_|`]
+  const mapBottom = [`|_|`, `..|`, `|_.`, `._|`, `..|`, `._|`, `|_|`, `..|`, `|_|`, `..|`]
+  const arr = [...str]
+
+  let resTop = ``
+  let resCenter = ``
+  let resBottom = ``
+  arr.forEach((cur, index) => {
+    if (index === arr.length - 1) {
+      resTop += mapTop[cur]
+      resCenter += mapCenter[cur]
+      resBottom += mapBottom[cur]
+    } else {
+      resTop += mapTop[cur] + ` `
+      resCenter += mapCenter[cur] + ` `
+      resBottom += mapBottom[cur] + ` `
+    }
+  })
+  let res = resTop + `\n` + resCenter + `\n` + resBottom + `\n`
+  return res
 };
+
